dab58424c725e06cb676ce3ad86fe0f2dea5dc7b
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2018-07-25 14:08:22 +0200

    Dice roller1 completed

--- Dice_roller/Program.cs
+++ Dice_roller/Program.cs
@@ -11,33 +11,35 @@ namespace Dice_roller
     {
         private static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-
             Regex myRegex = new Regex(@"^([1-9][0-9]{0,1}|100)d([1-9][0-9]|100|[2-9])\b$");
-            Match match = myRegex.Match(input);
 
-            if (match.Success)
+            while (true)
             {
-                Console.WriteLine("here we go");
-                Console.WriteLine("throw the dice {0} times, {1}-sided dice",match.Groups[1], match.Groups[2]);
-                Random random = new Random();
+                Console.Write("Enter input [no_of_dices]d[how many sides the dice has]: ");
+                string input = Console.ReadLine();
+                Match match = myRegex.Match(input);
 
-                StringBuilder sb = new StringBuilder();
-                int counter = 0;
-
-                for (int i = 0; i < Int32.Parse(match.Groups[1].Value); i++)
+                if (match.Success)
                 {
-                    var result = random.Next(1, Int32.Parse(match.Groups[2].Value));
-                    sb.Append(result).Append(' ');
-                    counter += result;
+                    Console.WriteLine("throw the dice {0} times, {1}-sided dice", match.Groups[1], match.Groups[2]);
+                    Random random = new Random();
+
+                    StringBuilder sb = new StringBuilder();
+                    int counter = 0;
+
+                    for (int i = 0; i < Int32.Parse(match.Groups[1].Value); i++)
+                    {
+                        var result = random.Next(1, Int32.Parse(match.Groups[2].Value));
+                        sb.Append(result).Append(' ');
+                        counter += result;
+                    }
+                    Console.WriteLine("{0}: {1}", counter, sb);
                 }
-                Console.WriteLine("{0}: {1}", counter, sb);
+                else
+                {
+                    Console.WriteLine("Enter valid data.");
+                }
             }
-            else
-            {
-                Console.WriteLine("huja wyszlo");
-            }
-            Console.ReadKey();
         }
     }
 }
