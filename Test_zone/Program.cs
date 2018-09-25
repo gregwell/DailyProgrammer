30a38179d8ffca043c9a1b891481cb1404c6fd80
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2018-09-25 12:27:46 +0200

    renamed

--- Test_zone/Program.cs
+++ Test_zone/Program.cs
@@ -1,31 +1,169 @@
-class Program
+using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace DotNetDesignPatternDemos.SOLID.OCP
+{
+    public enum Color
     {
-        static void Main(string[] args)
-        {
-            List<Employee> Employees = new List<Employee>() { 
-                new Employee(5112, "John", 1000.34),
-                new Employee(5113, "Kennedy", 20045.10),
-                new Employee(5115, "John", 5000.23),
-                new Employee(5116, "Anna", 32400.43)
-            };
-        }
+        Red, Green, Blue
     }
-    public class Employee
+
+    public enum Size
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public double salary { get; set; }
-        public Employee(int _id, string _name, double _salary)
+        Small, Medium, Large, Yuge
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
         {
-            id = _id;
-            name = _name;
-            salary = _salary;
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Color = color;
+            Size = size;
         }
     }
 
-var empList = Employees.Where(emp => emp.name == "John");
-           foreach(var emp in empList){
-               Console.WriteLine(emp.id);
-               // 5112
-               // 5115
-           }
+    public class ProductFilter
+    {
+        // let's suppose we don't want ad-hoc queries on products
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+
+        public static IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+        } // state space explosion
+
+        // 3 criteria = 7 methods
+
+        // OCP = open for extension but closed for modification
+    }
+
+    /// / / //
+    /// //  
+    /// / /// 
+    /// ////
+    // we introduce two new interfaces that are open for extension
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(Product p);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Size == size;
+        }
+    }
+
+    // combinator
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return first.IsSatisfied(p) && second.IsSatisfied(p);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+    }
+
+    public class Demo
+    {
+        private static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+                WriteLine($" - {p.Name} is green");
+
+            // ^^ BEFORE
+
+            // vv AFTER
+            var bf = new BetterFilter();
+            WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                WriteLine($" - {p.Name} is green");
+
+            WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                WriteLine($" - {p.Name} is large");
+
+            WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+              new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))
+            )
+            {
+                WriteLine($" - {p.Name} is big and blue");
+            }
+        }
+    }
+}
