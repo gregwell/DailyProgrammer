27e56e23bf8b2d34e4cc72eea6fff2386d418ab4
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2018-09-25 21:55:15 +0200

    taking input

--- 355 - Alphabet Cipher/Program.cs
+++ 355 - Alphabet Cipher/Program.cs
@@ -21,22 +21,41 @@ namespace Alphabet_Cipher
 
             //THINGS TO DO:
             //1) take user input
+            Console.WriteLine("Sample input: keyword messagetoencode");
+            Console.Write("Your input: ");
 
             //2) divide it to two parts ( separator - space )
-            //  keyword = train
-            //  message = murderontheorientexpress
+            string[] input = Console.ReadLine().Trim().Split(' ');
 
             //3) count message letters and extend keyword as much as needed so
             //  keyword = traintraintraintraintrai
             //  message = murderontheorientexpress
 
+            var keyword = input[0];
+
+            Console.WriteLine(input[1].Length / input[0].Length);
+            Console.WriteLine(input[1].Length % input[0].Length);
+
+            char[] k = input[0].ToCharArray();
+
+            for (int i = 1; i < (input[1].Length / keyword.Length); i++)
+            {
+                input[0] += keyword;
+            }
+
+            for (int i = 0; i < (input[1].Length % keyword.Length); i++)
+            {
+                input[0] += k[i];
+            }
+
+            Console.WriteLine(input[0]);
+
             //4) make arrays from those strings where every letter is separated.
+
+            Console.ReadLine();
 
             //5) figure out how to multiply these two letters to have the result...
             //    k[i] * m[i] = result[i]
-
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
         }
     }
 }
