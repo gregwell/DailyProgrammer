b31f3d466bc083c6047cf99e156d19ca5d2cc51a
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2018-09-25 15:27:17 +0200

    working plan

--- 355 - Alphabet Cipher/Program.cs
+++ 355 - Alphabet Cipher/Program.cs
@@ -10,6 +10,31 @@ namespace Alphabet_Cipher
     {
         private static void Main(string[] args)
         {
+            //Substitution chart
+            //result - the result of the substrition chart
+            //first- letter of my keyword
+            //second- letter of the message that is being encoded
+            //result = first * second
+
+            //CHALLENGE INPUT
+            // train murderontheorientexpress\
+
+            //THINGS TO DO:
+            //1) take user input
+
+            //2) divide it to two parts ( separator - space )
+            //  keyword = train
+            //  message = murderontheorientexpress
+
+            //3) count message letters and extend keyword as much as needed so
+            //  keyword = traintraintraintraintrai
+            //  message = murderontheorientexpress
+
+            //4) make arrays from those strings where every letter is separated.
+
+            //5) figure out how to multiply these two letters to have the result...
+            //    k[i] * m[i] = result[i]
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
