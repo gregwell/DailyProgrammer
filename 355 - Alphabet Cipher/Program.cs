4d3707a54583f5a6a4999fa59804b46bf17bcdb3
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2018-09-28 15:55:58 +0200

    regex to validate user input implemented

--- 355 - Alphabet Cipher/Program.cs
+++ 355 - Alphabet Cipher/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Alphabet_Cipher
 {
@@ -11,29 +12,42 @@ namespace Alphabet_Cipher
         private static void Main(string[] args)
         {
             //collect user input and divide to two strings.
-            Console.WriteLine("Sample input: keyword messagetoencode");
+            Console.WriteLine("Sample input: keyword_default messagetoencode");
             Console.Write("Your input: ");
-            string[] input = Console.ReadLine().Trim().Split(' ');
+            string input = Console.ReadLine();
 
-            //extend keyword to length of message.
-            var keyword = input[0];
-            char[] kposition = input[0].ToCharArray();
-            for (int i = 1; i < (input[1].Length / keyword.Length); i++) input[0] += keyword;
-            for (int i = 0; i < (input[1].Length % keyword.Length); i++) input[0] += kposition[i];
+            // regex
+            var myRegex = new Regex(@"\b([a-z]+)\s([a-z]+)\b");
+            Match match = myRegex.Match(input);
+
+            while (!match.Success)
+            {
+                Console.WriteLine("Enter correct input");
+                input = Console.ReadLine();
+                match = myRegex.Match(input);
+            }
+
+            var keyword = match.Groups[1].Value;
+            var message = match.Groups[2].Value;
+            //extend keyword_default to length of message.
+            var keyword_default = keyword;
+            char[] kposition = keyword.ToCharArray();
+            for (int i = 1; i < (keyword.Length / keyword_default.Length); i++) keyword += keyword_default;
+            for (int i = 0; i < (message.Length % keyword_default.Length); i++) keyword += kposition[i];
 
             //make char arrays k and m.
-            char[] k = input[0].ToCharArray();
-            char[] m = input[1].ToCharArray();
+            char[] k = keyword.ToCharArray();
+            char[] m = message.ToCharArray();
 
             char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower().ToCharArray();
 
-            int[] k_index = new int[input[0].Length];
-            int[] m_index = new int[input[0].Length];
+            int[] k_index = new int[keyword.Length];
+            int[] m_index = new int[keyword.Length];
 
             //searching for alphabet index number of my letters
-            for (int i = 0; i < input[0].Length; i++)
+            for (int i = 0; i < keyword.Length; i++)
             {
-                for (int j = 0; j < input[0].Length; j++)
+                for (int j = 0; j < keyword.Length; j++)
                 {
                     int real_i = i;
                     if (i > 25) i = (i % 25 - 1);
@@ -45,9 +59,9 @@ namespace Alphabet_Cipher
             }
 
             //display changed letters
-            int[] result_index = new int[input[0].Length];
+            int[] result_index = new int[keyword.Length];
 
-            for (int i = 0; i < input[0].Length; i++)
+            for (int i = 0; i < keyword.Length; i++)
             {
                 result_index[i] = k_index[i] + m_index[i];
                 if (result_index[i] > 25) result_index[i] = (result_index[i] % 25 - 1);
